93b2ccfd2bd1c1b64da842baf0d69291f1949393
Ramon Zambrano <170786939+r-alejo-z95@users.noreply.github.com>
2025-04-08 21:53:19 -0500

    Integrate Clerk authentication and create layout for sign-in and sign-up pages

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,10 @@
 import { ThemeProvider } from "@/components/theme-provider";
-import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import { Header } from "@/components/header";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import "./globals.css";
 
 export const metadata = {
   title: "MentorLink - Empowering Mentors",
@@ -14,27 +15,29 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTranstionOnChange
-        >
-          <Header />
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTranstionOnChange
+          >
+            <Header />
 
-          <main className="min-h-screen shadow-md">{children}</main>
+            <main className="min-h-screen shadow-md">{children}</main>
 
-          <footer className="py-8">
-            <div className="container mx-auto px-4 text-center text-primary">
-              <Link href="https://github.com/r-alejo-z95">
-                <Button variant="link">Made by Ramon Z.</Button>
-              </Link>
-            </div>
-          </footer>
-        </ThemeProvider>
-      </body>
-    </html>
+            <footer className="py-8">
+              <div className="container mx-auto px-4 text-center text-primary">
+                <Link href="https://github.com/r-alejo-z95">
+                  <Button variant="link">Made by Ramon Z.</Button>
+                </Link>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
